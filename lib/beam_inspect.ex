cd572bec850b2a51f97a57d7d7a12d7bfea78188
Tobiasz <tobiasz.malecki@appunite.com>
2018-06-08 19:21:57 +0200

    docs & core erlang formatting option

--- lib/beam_inspect.ex
+++ lib/beam_inspect.ex
@@ -1,14 +1,59 @@
 defmodule BeamInspect do
+  @moduledoc """
+  See how your elixir module looks like in erlang/core erlang.
+
+  It requires `:debug_info` or `:abstract_code` in .beam file.
+  """
+
+  @doc """
+  Returns erlang abstract code
+
+  ## Example
+
+      iex > Foo |> BeamInspect.to_erlang() |> IO.puts()
+      :ok
+
+  """
+  @spec to_erlang(module()) :: charlist() | no_return()
   def to_erlang(module) when is_atom(module) do
     module
     |> abstract_code()
     |> to_erl()
   end
 
-  def to_core_erlang(module, erlc_flags \\ []) when is_atom(module) do
+  @format_opts [:noann]
+
+  @doc """
+  Returns core erlang code (erlang abstract code compiled with `+to_core` flag)
+
+  ## Options
+
+    * erlc flags - e.g. +time should be passed as `:time` atom
+
+    * `:noann` - removes compiler annotations
+
+  ## Examples
+
+      iex > Foo |> BeamInspect.to_core_erlang() |> IO.puts()
+      :ok
+
+      iex > Foo |> BeamInspect.to_core_erlang(:noann) |> IO.puts()
+      :ok
+
+      iex > Foo |> BeamInspect.to_core_erlang(:time) |> IO.puts()
+      :ok
+
+      iex > Foo |> BeamInspect.to_core_erlang([:noann, :time]) |> IO.puts()
+      :ok
+
+  """
+  @spec to_core_erlang(module(), atom() | [atom()]) :: charlist() | no_return()
+  def to_core_erlang(module, opts \\ []) when is_atom(module) do
+    {format_opts, erlc_flags} = opts |> List.wrap() |> Enum.split_with(&(&1 in @format_opts))
+
     module
     |> abstract_code()
-    |> to_core(erlc_flags)
+    |> to_core(erlc_flags, :noann in format_opts)
   end
 
   defp abstract_code(module) do
@@ -33,9 +78,9 @@ defmodule BeamInspect do
     |> :erl_prettypr.format()
   end
 
-  defp to_core(abstract_code, erlc_flags) do
+  defp to_core(abstract_code, erlc_flags, noann) do
     {:ok, _, core} = :compile.noenv_forms(abstract_code, [:to_core | erlc_flags])
 
-    :cerl_prettypr.format(core)
+    :cerl_prettypr.format(core, noann: noann)
   end
 end
